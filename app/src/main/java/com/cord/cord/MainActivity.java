b794f1a427f42a9b4b5a2e843853f8319674e527
Ever Barillas <everalf1995@gmail.com>
2018-12-17 21:17:51 -0500

    Initial commit

---
+++ app/src/main/java/com/cord/cord/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cord.cord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
