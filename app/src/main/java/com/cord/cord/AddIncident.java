09fb92de5461107dd292e4e90685bc746ae964a8
Ever Barillas <everalf1995@gmail.com>
2018-12-17 21:21:20 -0500

    Created new activity to add incidents

---
+++ app/src/main/java/com/cord/cord/AddIncident.java
@@ -0,0 +1,13 @@
+package com.cord.cord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddIncident extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_incident);
+    }
+}
