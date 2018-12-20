98919862179c048c4f28b9b2d7b02c40fbfd421e
Ever Barillas <everalf1995@gmail.com>
2018-12-20 01:35:44 -0500

    - Created splash screen - Added transition animations - Added new placeholder logo - Added new splash screen background

---
+++ app/src/main/java/com/cord/cord/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.cord.cord;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent LandingActivityIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(LandingActivityIntent);
+                overridePendingTransition(R.anim.enter_from_right, R.anim.exit_to_left);
+                finish();
+            }
+        }, 500);
+    }
+}
